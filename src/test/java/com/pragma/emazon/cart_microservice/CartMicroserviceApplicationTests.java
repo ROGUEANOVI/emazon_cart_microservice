d44ec465c14a7185f952b62e0841f1ec3c9fc3f6
rogueanovi <ovidioromero66@gmail.com>
2024-09-15 17:02:18 -0500

    first commit

---
+++ src/test/java/com/pragma/emazon/cart_microservice/CartMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.emazon.cart_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
